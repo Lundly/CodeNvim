d5e3fdd275447b3feb45ddeb56684dd1d2f73c00
Lundly <Lundlymoonlee@163.com>
2026-07-25 18:50:04 +0800

    refactor(bufferline.lua): 配置bufferline的lsp支持

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,8 +5,27 @@ return {
     -- config session will execute when plugin loads
     ---@type fun(plugin, opts: table)
     config = function()
-      require("bufferline").setup({
+      local bufferline = require("bufferline")
+      bufferline.setup({
         options = {
+          separator_style = "thick",
+          numbers = "none",
+          indicator = {
+            icon = "▎",
+            style = "underline" -- "icon" | "underline" | "none"
+          },
+          diagnostics = "nvim_lsp",
+          diagnostics_indicator = function (_, _, diag)
+            local icons = {
+              ERROR = " ",
+              WARN = " "
+            }
+            local sign = ( diag.error and icons.ERROR .. diag.error .. " " or "" )
+              .. ( diag.warning and icons.WARN .. diag.warning or "" )
+            return  vim.trim(sign)
+          end,
+
+          -- neo-tree
           mode = "buffers",
           offsets = {
             {

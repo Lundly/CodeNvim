958f652f4f53590af78f0d476e3b57f1bba11fb9
Lundly <Lundlymoonlee@163.com>
2026-07-28 16:39:12 +0800

    refactor: 调整部分插件的启动时机，调整minimap的显示样式
    启动时机优化：
    dropbar ->   ft = "*"
    snipper ->   event = { "BufReadPre", "BufNewFile" },
    mason   ->   event = "VeryLazy",
    
    minimap显示调整：
    调整语法检查显示图标，添加一些注释

--- lua/plugins/neominimap.lua
+++ lua/plugins/neominimap.lua
@@ -5,13 +5,33 @@ return {
     init = function ()
       vim.g.neominimap = {
         auto_enable = true,
-        layout = "float",
+        -- minimap style
+        layout = "float",-- include "float" | "split", "split" have to quit twice when execute ":q"
+        -- cursor action
         click = {
           enabled = true
         },
+        -- mark sign
         mark = {
           enabled = true
         },
+        diagnostic = {
+          enabled =  true,
+          mode = "icon", ---@type string include "line" | "sign" | "icon"
+          priority = {
+            ERROR = 100, ---@type integer
+            WARN  = 90, ---@type integer
+            INFO  = 80, ---@type integer
+            HINT  = 70, ---@type integer
+          },
+          icon = {
+              ERROR = "▎", ---@type string
+              WARN  = "▎", ---@type string
+              INFO  = "▎", ---@type string
+              HINT  = "▎", ---@type string
+          },
+        },
+        curren_line_pistion = "center",
       }
     end
   }

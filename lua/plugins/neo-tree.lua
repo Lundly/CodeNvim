af0736afe5261fdef4ce20f3c0cd3b18bd829e98
Lundly <Lundlymoonlee@163.com>
2026-07-28 18:57:11 +0800

    feat(neo-tree): 配置neo-tree，修改部分样式

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -13,5 +13,68 @@ return {
         "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Buffers (root dir)",
       },
     },
+    opts = {
+      source_selector = {
+        winbar = true,
+        statusline = false,
+        sources = {
+          {
+            source = "filesystem",
+            display_name = " 󰉓 Files "
+          },
+          {
+            source = "buffers",
+            display_name = " 󰈚 Buffers "
+          },
+          {
+            source = "git_status",
+            display_name = " 󰊢 Git "
+          },
+        },
+        separator = { left = "▏", right= "▕" },
+        separator_active = { left = "┃" },
+        show_separator_on_edge = true,
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true
+        },
+      },
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            -- change type
+            added     = "",
+            deleted   = " ",
+            modified  = " ",
+            renamed   = "󰁕 ",
+            -- Status type
+            untracked = " ",
+            ignored   = " ",
+            unstaged  = "U",
+            staged    = "󱓳 ",
+            conflict  = " ",
+          }
+        },
+        diagnostics = {
+          symbols = {
+            error = " ",
+            warn  = " ",
+            hint  = "󰌵 ",
+            info  = " ",
+          },
+          highlights = {
+            error = "DiagnosticSignError",
+            warn  = "DiagnosticSignWarn",
+            hint  = "DiagnosticSignHint",
+            info  = "DiagnosticSignInfo",
+          },
+        }
+      },
+    },
+    -- config = function (_, opts)
+    --   local nt = require("neo-tree")
+    --   nt.setup(opts.options)
+    -- end
   }
 }

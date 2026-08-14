6e975efb7f8acc04f42df3279cf3d8ba71925bbf
Lundly <Lundlymoonlee@163.com>
2026-08-14 17:59:38 +0800

    feat(colorscheme): 修改everforest主题当前行号颜色

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,6 +11,13 @@ return {
       vim.g.everforest_diagnostic_text_highlight = 1
       vim.g.everforest_current_word = "high contrast background"
       vim.g.everforest_enable_italic = 1
+      -- 当切换到 everforest 主题时，自动设置当前行号颜色
+      vim.api.nvim_create_autocmd("ColorScheme", {
+          pattern = "everforest",
+          callback = function()
+              vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#E69875", bold = true })
+          end,
+      })
     end
   },
 

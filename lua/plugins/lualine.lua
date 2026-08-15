d7af3887d527c144f424c4708b523a4d1fcc0689
Lundly <Lundlymoonlee@163.com>
2026-08-15 11:48:29 +0800

    fix(statusline): 修复不带参数时启动neovim状态栏状态参数错误问题
    当带有文件参数启动Neovim时不会启动snacks-dashboard，这时options.lua中的vim.opt.laststatus选项能够正常配置
    
    当不带文件参数直接启动Neovim时会启动snacks-dashboard，这与lualine的启动加载冲突导致laststatus的值最终为2
    
    修复部分，在lualine的配置中的options选项中加入globalstatus =
    true即可解决问题。
    
    此次提交同时修改了lualine的配置结构。

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,65 +4,64 @@ return {
     -- enabled = false,
     event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          icons_enable = true,
-          theme = "auto",
-          disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
-          component_separators = { left = "", right = ""},
-          section_separators = { left = "", right = ""},
+    opts = {
+      options = {
+        globalstatus = true,
+        icons_enable = true,
+        theme = "auto",
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+        component_separators = { left = "", right = ""},
+        section_separators = { left = "", right = ""},
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          "branch",
+          {
+            "diff",
+            symbols = {
+              added = Core.configs.icons.git.Added,
+              modified = Core.configs.icons.git.Modified,
+              removed = Core.configs.icons.git.Deleted,
+            }
+          },
         },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = {
-            "branch",
-            {
-              "diff",
-              symbols = {
-                added = Core.configs.icons.git.Added,
-                modified = Core.configs.icons.git.Modified,
-                removed = Core.configs.icons.git.Deleted,
-              }
+        lualine_c = {
+          {
+            "diagnostics",
+            symbols = {
+              error = Core.configs.icons.diagnostics.Error,
+              warn  = Core.configs.icons.diagnostics.Warn,
+              info  = Core.configs.icons.diagnostics.Info,
+              hint  = Core.configs.icons.diagnostics.Hint,
             },
           },
-          lualine_c = {
-            {
-              "diagnostics",
-              symbols = {
-                error = Core.configs.icons.diagnostics.Error,
-                warn  = Core.configs.icons.diagnostics.Warn,
-                info  = Core.configs.icons.diagnostics.Info,
-                hint  = Core.configs.icons.diagnostics.Hint,
-              },
-            },
-            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            "filename",
-          },
-          lualine_x = {
-             {
-              -- showcmd
-              function() return require("noice").api.status.command.get() end,
-              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-            },
-            "encoding",
-            function()
-              return "Indent " .. vim.bo.shiftwidth
-            end,
-          },
-          lualine_y = {
-            { "progress", separator = "" , padding = { left = 1, right = 0 } },
-            "location",
-          },
-          lualine_z = {
-            -- current time
-            function()
-              return " " .. os.date("%R")
-            end,
-          }
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          "filename",
         },
-        extensions = { "neo-tree", "fzf", "lazy" },
-      })
-    end
+        lualine_x = {
+           {
+            -- showcmd
+            function() return require("noice").api.status.command.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+          },
+          "encoding",
+          function()
+            return "Indent " .. vim.bo.shiftwidth
+          end,
+        },
+        lualine_y = {
+          { "progress", separator = "" , padding = { left = 1, right = 0 } },
+          "location",
+        },
+        lualine_z = {
+          -- current time
+          function()
+            return " " .. os.date("%R")
+          end,
+        }
+      },
+      extensions = { "neo-tree", "fzf", "lazy" },
+    },
   }
 }
